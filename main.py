2b9045546c47471adbc6b6e0ff77f3849d0fafc1
Rahul <rahulmathur87@gmail.com>
2024-08-14 06:51:47 +0530

    add flask-sqlalchemy for permanent database

--- main.py
+++ main.py
@@ -1,4 +1,52 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+
+app = Flask(__name__)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    with app.app_context():
+        result = db.session.execute(db.select(Book).order_by(Book.title))
+        all_books = result.scalars().all()
+    return render_template('index.html', books=all_books)
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == "POST":
+        with app.app_context():
+            new_book = Book(title=request.form['title'], author=request.form['author'], rating=request.form['rating'])
+            db.session.add(new_book)
+            db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 '''
 Red underlines? Install the required packages first: 
@@ -12,29 +60,3 @@ pip3 install -r requirements.txt
 
 This will install the packages from requirements.txt for this project.
 '''
-
-app = Flask(__name__)
-
-all_books = []
-
-
-@app.route('/')
-def home():
-    return render_template('index.html', books=all_books)
-
-
-@app.route("/add", methods=['GET', 'POST'])
-def add():
-    if request.method == "POST":
-        new_book = {
-            'title': request.form['title'],
-            'author': request.form['author'],
-            'rating': request.form['rating']
-        }
-        all_books.append(new_book)
-        return redirect(url_for('home'))
-    return render_template('add.html')
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
